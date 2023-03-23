f01df6b3d1087fc0d6c50b06d3c3e06007e77f5c
Udi Silvestre <udi.silvestre@strapi.io>
2023-03-23 18:26:22 -0300

    Update Home page Carousel component

--- docusaurus/src/components/Carousel/Carousel.jsx
+++ docusaurus/src/components/Carousel/Carousel.jsx
@@ -1,25 +1,67 @@
 import clsx from 'clsx';
-import React, { useRef } from 'react';
-import { register as swiperWebComponentsRegister } from 'swiper/element/bundle';
+import React, { useCallback, useEffect, useState } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import EmblaPluginAutoplay from 'embla-carousel-autoplay';
+import IconChevronLeft from '@site/static/img/assets/icons/chevron-left.svg';
+import IconChevronRight from '@site/static/img/assets/icons/chevron-right.svg';
 import styles from './carousel.module.scss';
 
-swiperWebComponentsRegister();
-
-export function CarouselSlide(props) {
+export function CarouselSlide({
+  className,
+  ...rest
+}) {
   return (
-    <swiper-slide
-      {...props}
+    <div
+      className={clsx(
+        styles.carousel__slide,
+        className,
+      )}
+      {...rest}
     />
   );
 }
 
 export function Carousel({
-  children,
   className,
-  ref = null,
+  options,
   ...rest
 }) {
-  const swiperElRef = useRef(ref);
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [scrollSnaps, setScrollSnaps] = useState([])
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
+    ...(options || {}),
+  }, [
+    EmblaPluginAutoplay({
+      delay: 10000,
+    }),
+  ]);
+
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi],
+  )
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi],
+  )
+  const scrollTo = useCallback(
+    (index) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi],
+  )
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return
+    setSelectedIndex(emblaApi.selectedScrollSnap())
+  }, [emblaApi, setSelectedIndex])
+
+  useEffect(() => {
+    if (!emblaApi) return
+    onSelect()
+    setScrollSnaps(emblaApi.scrollSnapList())
+    emblaApi.on('select', onSelect)
+    emblaApi.on('reInit', onSelect)
+  }, [emblaApi, setScrollSnaps, onSelect])
 
   return (
     <div
@@ -28,17 +70,49 @@ export function Carousel({
         className,
       )}
     >
-      <swiper-container
-        ref={swiperElRef}
-        slides-per-view={1}
-        // autoplay
-        loop
-        navigation
-        pagination
-        {...rest}
+      <div
+        ref={emblaRef}
+        className={clsx(
+          styles.carousel__viewport,
+        )}
       >
-        {children}
-      </swiper-container>
+        <div
+          className={clsx(
+            styles.carousel__container,
+          )}
+          {...rest}
+        />
+      </div>
+      <button
+        onClick={scrollPrev}
+        className={clsx(
+          styles.carousel__arrow,
+          styles['carousel__arrow--prev'],
+        )}
+      >
+        <IconChevronLeft />
+      </button>
+      <button
+        onClick={scrollNext}
+        className={clsx(
+          styles.carousel__arrow,
+          styles['carousel__arrow--next'],
+        )}
+      >
+        <IconChevronRight />
+      </button>
+      <div className={styles.carousel__pagination}>
+        {scrollSnaps.map((_, paginationItemIndex) => (
+          <button
+            key={paginationItemIndex}
+            onClick={() => scrollTo(paginationItemIndex)}
+            className={clsx(
+              styles.carousel__pagination__btn,
+              (paginationItemIndex === selectedIndex && styles['carousel__pagination__btn--active'])
+            )}
+          />
+        ))}
+      </div>
     </div>
   );
 }
